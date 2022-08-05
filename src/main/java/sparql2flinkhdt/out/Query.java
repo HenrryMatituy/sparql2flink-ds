4347b3aa549ca08b37f2a93f7da9135c8fa2ecb6
henrry <henrrymatituy@gmail.com>
2022-08-05 15:44:40 -0500

    .

--- src/main/java/sparql2flinkhdt/out/Query.java
+++ src/main/java/sparql2flinkhdt/out/Query.java
@@ -1,3 +1,4 @@
+
 package sparql2flinkhdt.out;
 
 import org.apache.flink.api.java.DataSet;
@@ -16,48 +17,48 @@ import org.apache.jena.graph.Triple;
 import sparql2flinkhdt.runner.functions.*;
 import sparql2flinkhdt.runner.LoadTriples;
 import sparql2flinkhdt.runner.functions.order.*;
+
 import java.math.*;
+
 import org.apache.flink.api.common.RuntimeExecutionMode;
 
 public class Query {
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		final ParameterTool params = ParameterTool.fromArgs(args);
+        final ParameterTool params = ParameterTool.fromArgs(args);
 
-		if (!params.has("dataset") && !params.has("output")) {
-			System.out.println("Use --dataset to specify dataset path and use --output to specify output path.");
-		}
+        if (!params.has("dataset") && !params.has("output")) {
+            System.out.println("Use --dataset to specify dataset path and use --output to specify output path.");
+        }
 
-		//************ Environment (DataSet) and Source (static RDF dataset) ************
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		env.setRuntimeMode(RuntimeExecutionMode.BATCH);
-		DataStream<Triple> datastream = LoadTriples.fromDataset(env, params.get("dataset"));
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setRuntimeMode(RuntimeExecutionMode.BATCH);
+        DataStream<Triple> datastream = LoadTriples.fromDataset(env, params.get("dataset"));
 
-		//************ Applying Transformations ************
-		DataStream<SolutionMapping> sm1 = datastream
-			.filter(new Triple2Triple(null, "http://xmlns.com/foaf/0.1/name", null))
-			.map(new Triple2SolutionMapping("?person", null, "?name"));
+        DataStream<SolutionMapping> sm1 = datastream
+                .filter(new Triple2Triple(null, "http://xmlns.com/foaf/0.1/name", null))
+                .map(new Triple2SolutionMapping("?person", null, "?name"));
 
-		DataStream<SolutionMapping> sm2 = datastream
-			.filter(new Triple2Triple(null, "http://xmlns.com/foaf/0.1/mbox", null))
-			.map(new Triple2SolutionMapping("?person", null, "?mbox"));
+        DataStream<SolutionMapping> sm2 = datastream
+                .filter(new Triple2Triple(null, "http://xmlns.com/foaf/0.1/mbox", null))
+                .map(new Triple2SolutionMapping("?person", null, "?mbox"));
 
 
-		DataStream<SolutionMapping> sm3 = sm1.coGroup(sm2)
-				.where(new JoinKeySelector(new String[]{"?person"}))
-				.equalTo(new JoinKeySelector(new String[]{"?person"}))
-				.window(GlobalWindows.create())
-				.trigger(CountTrigger.of(5))
-				.apply (new CoGroup());
+        DataStream<SolutionMapping> sm3 = sm1.coGroup(sm2)
+                .where(new JoinKeySelector(new String[]{"?person"}))
+                .equalTo(new JoinKeySelector(new String[]{"?person"}))
+                .window(GlobalWindows.create())
+                .trigger(CountTrigger.of(5))
+                .apply(new CoGroup());
 
 
-		DataStream<SolutionMapping> sm4 = sm3
-			.map(new Project(new String[]{"?person", "?name", "?mbox"}));
+        DataStream<SolutionMapping> sm4 = sm3
+                .map(new Project(new String[]{"?person", "?name", "?mbox"}));
 
-		//************ Sink  ************
-		sm4.writeAsText(params.get("output")+"Query-Flink-Result", FileSystem.WriteMode.OVERWRITE)
-			.setParallelism(1);
+        //************ Sink  ************
+        sm4.writeAsText(params.get("output") + "Query-Flink-Result", FileSystem.WriteMode.OVERWRITE)
+                .setParallelism(1);
 
-		env.execute("SPARQL Query to Flink Progran - DataStream API");
-	}
+        env.execute("SPARQL Query to Flink Progran - DataStream API");
+    }
 }
