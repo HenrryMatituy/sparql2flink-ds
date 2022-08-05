afafbd47644d9f7e6d17e1c69ceb47b959353895
henrry <henrrymatituy@gmail.com>
2022-08-04 21:43:31 -0500

    .

--- src/main/java/sparql2flinkhdt/runner/functions/CoGroup.java
+++ src/main/java/sparql2flinkhdt/runner/functions/CoGroup.java
@@ -1,15 +1,28 @@
 package sparql2flinkhdt.runner.functions;
 
-import org.apache.flink.api.common.functions.FlatJoinFunction;
 import org.apache.flink.util.Collector;
 import org.apache.flink.api.common.functions.CoGroupFunction;
-import sparql2flinkhdt.runner.functions.SolutionMapping;
+import org.apache.jena.graph.Node;
 
-//SolutionMapping - Flat Join Function
+import java.util.Map;
+
 public class CoGroup implements CoGroupFunction<SolutionMapping, SolutionMapping, SolutionMapping> {
-
     @Override
-     public void coGroup(Iterable<SolutionMapping> left, Iterable<SolutionMapping> right, Collector<SolutionMapping> out) throws Exception {
-        out.collect(left.iterator().next());
-    }
+    public void coGroup(Iterable<SolutionMapping> left, Iterable<SolutionMapping> right, Collector<SolutionMapping> out) throws Exception {
+//    out.collect(left.iterator().next());
+//        out.collect(right.CoGroupp(left));
+
+          }
+
+//    public CoGroup coGroupp(SolutionMapping sm) {
+//        if(sm != null) {
+//            for (Map.Entry<String, Node> hm : sm.getMapping().entrySet()) {
+//                if (!existMapping(hm.getKey(), hm.getValue())) {
+//                    this.putMapping(hm.getKey(), hm.getValue());
+//                }
+//            }
+//        }
+//        return this;
+//    }
 }
+
